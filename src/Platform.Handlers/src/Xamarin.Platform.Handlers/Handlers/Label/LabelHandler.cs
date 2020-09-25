9fd866e551eca8a7080df15d25933409ec20d04c
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-09-25 10:05:38 +0200

    Refactored LabelHandler

--- src/Platform.Handlers/src/Xamarin.Platform.Handlers/Handlers/Label/LabelHandler.cs
+++ src/Platform.Handlers/src/Xamarin.Platform.Handlers/Handlers/Label/LabelHandler.cs
@@ -1,24 +1,115 @@
-﻿namespace Xamarin.Platform.Handlers
+﻿#if __IOS__
+using RectangleF = CoreGraphics.CGRect;
+using NativeView = UIKit.UILabel;
+#elif __MACOS__
+using NativeView = AppKit.NSTextField;
+#elif MONOANDROID
+using NativeView = Android.Widget.TextView;
+#elif NETCOREAPP
+using NativeView = System.Windows.Controls.TextBlock;
+#elif NETSTANDARD
+using NativeView = System.Object;
+# endif
+
+namespace Xamarin.Platform.Handlers
 {
-	public partial class LabelHandler
+	public partial class LabelHandler : AbstractViewHandler<ILabel, NativeView>
 	{
-		public static PropertyMapper<ILabel> LabelMapper = new PropertyMapper<ILabel>(ViewHandler.ViewMapper)
+		public static PropertyMapper<ILabel, LabelHandler> LabelMapper = new PropertyMapper<ILabel, LabelHandler>(ViewHandler.ViewMapper)
 		{
-			[nameof(ILabel.Text)] = MapPropertyText,
-			[nameof(ILabel.TextColor)] = MapPropertyTextColor,
-			[nameof(ILabel.LineHeight)] = MapPropertyLineHeight,
-			[nameof(ILabel.Font)] = MapPropertyFont,
-			[nameof(ILabel.FontSize)] = MapPropertyFontSize,
-			[nameof(ILabel.FontAttributes)] = MapPropertyFontAttributes,
-			[nameof(ILabel.TextTransform)] = MapPropertyTextTransform,
-			[nameof(ILabel.HorizontalTextAlignment)] = MapPropertyHorizontalTextAlignment,
-			[nameof(ILabel.VerticalTextAlignment)] = MapPropertyVerticalTextAlignment,
-			[nameof(ILabel.CharacterSpacing)] = MapPropertyCharacterSpacing,
-			[nameof(ILabel.TextDecorations)] = MapPropertyTextDecorations,
-			[nameof(ILabel.LineBreakMode)] = MapPropertyLineBreakMode,
-			[nameof(ILabel.MaxLines)] = MapPropertyMaxLines,
-			[nameof(IPadding.Padding)] = MapPropertyPadding
+			[nameof(IText.Text)] = MapText,
+			[nameof(IText.TextColor)] = MapTextColor,
+			[nameof(IText.Font)] = MapFont,
+			[nameof(IText.TextTransform)] = MapTextTransform,
+			[nameof(IText.CharacterSpacing)] = MapCharacterSpacing,
+			[nameof(ILabel.LineHeight)] = MapLineHeight,
+			[nameof(ILabel.FontSize)] = MapFontSize,
+			[nameof(ILabel.FontAttributes)] = MapFontAttributes,
+			[nameof(ILabel.HorizontalTextAlignment)] = MapHorizontalTextAlignment,
+			[nameof(ILabel.VerticalTextAlignment)] = MapVerticalTextAlignment,
+			[nameof(ILabel.TextDecorations)] = MapTextDecorations,
+			[nameof(ILabel.LineBreakMode)] = MapLineBreakMode,
+			[nameof(ILabel.MaxLines)] = MapMaxLines,
+			[nameof(IPadding.Padding)] = MapPadding
 		};
+
+		public static void MapText(LabelHandler handler, ILabel label)
+		{
+
+		}
+
+		public static void MapTextColor(LabelHandler handler, ILabel label)
+		{
+
+		}
+
+		public static void MapFont(LabelHandler handler, ILabel label)
+		{
+
+		}
+
+		public static void MapTextTransform(LabelHandler handler, ILabel label)
+		{
+
+		}
+
+		public static void MapCharacterSpacing(LabelHandler handler, ILabel label)
+		{
+
+		}
+
+		public static void MapLineHeight(LabelHandler handler, ILabel label)
+		{
+
+		}
+
+		public static void MapFontSize(LabelHandler handler, ILabel label)
+		{
+
+		}
+
+		public static void MapFontAttributes(LabelHandler handler, ILabel label)
+		{
+
+		}
+
+		public static void MapHorizontalTextAlignment(LabelHandler handler, ILabel label)
+		{
+
+		}
+
+		public static void MapVerticalTextAlignment(LabelHandler handler, ILabel label)
+		{
+
+		}
+
+		public static void MapTextDecorations(LabelHandler handler, ILabel label)
+		{
+
+		}
+
+		public static void MapLineBreakMode(LabelHandler handler, ILabel label)
+		{
+
+		}
+
+		public static void MapMaxLines(LabelHandler handler, ILabel label)
+		{
+
+		}
+
+		public static void MapPadding(LabelHandler handler, ILabel label)
+		{
+
+		}
+
+#if MONOANDROID
+		protected override NativeView CreateView() => new NativeView(this.Context);
+#elif __IOS__
+		protected override NativeView CreateView() => new NativeView(RectangleF.Empty);
+#else
+		protected override NativeView CreateView() => new NativeView();
+#endif
 
 		public LabelHandler() : base(LabelMapper)
 		{
